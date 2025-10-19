b978b292afce6e0fda78f01988f3acfb75175b29
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-10-19 15:20:49 +0300

    Refactor server.js for cleaner syntax and structure

--- src/server.js
+++ src/server.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const contactsRouter = require('./routers/contacts');
-const notFoundHandler = require('./middlewares/notFoundHandler');
-const errorHandler = require('./middlewares/errorHandler');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const { notFoundHandler } = require('./middlewares/notFoundHandler')
+const { errorHandler } = require('./middlewares/errorHandler')
+const contactsRouter = require('./routers/contacts')
 
-const app = express();
-app.use(express.json());
-app.use('/contacts', contactsRouter);
-app.use(notFoundHandler);
-app.use(errorHandler);
+const app = express()
+app.use(express.json())
+
+app.use('/contacts', contactsRouter)
+
+app.use('*', notFoundHandler)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(process.env.PORT || 3000);
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   })
-  .catch((err) => console.error('Mongo connection error:', err.message));
+  .catch((err) => console.error('Mongo connection error:', err.message))
