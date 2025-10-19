0876b99bcb86627fd3d0cc957981083760c5b513
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-10-19 15:20:10 +0300

    Refactor contacts router to use express app

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const { getById, create, patch, remove } = require('../controllers/contacts');
-const ctrlWrapper = require('../utils/ctrlWrapper');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const { notFoundHandler } = require('./middlewares/notFoundHandler')
+const { errorHandler } = require('./middlewares/errorHandler')
+const contactsRouter = require('./routers/contacts')
 
-const router = express.Router();
+const app = express()
+app.use(express.json())
 
-router.get('/:contactId', ctrlWrapper(getById));
-router.post('/', ctrlWrapper(create));
-router.patch('/:contactId', ctrlWrapper(patch));
-router.delete('/:contactId', ctrlWrapper(remove));
+app.use('/contacts', contactsRouter)
 
-module.exports = router;
+app.use('*', notFoundHandler)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
+  .catch((err) => console.error('Mongo connection error:', err.message))
